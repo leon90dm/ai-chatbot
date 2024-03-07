09545d4a6266b38192180b4df319168cdba3144f
bilbodai <bilbodai@163.com>
2024-03-07 23:52:33 +0800

    no auth

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -28,33 +28,33 @@ export async function POST(req: Request) {
   //   temperature: 0.7,
   //   stream: true
   // })
-  const stream = OpenAIStream(res, {
-    async onCompletion(completion) {
-      const title = json.messages[0].content.substring(0, 100)
-      const id = json.id ?? nanoid()
-      const createdAt = Date.now()
-      const path = `/chat/${id}`
-      const payload = {
-        id,
-        title,
-        userId,
-        createdAt,
-        path,
-        messages: [
-          ...messages,
-          {
-            content: completion,
-            role: 'assistant'
-          }
-        ]
-      }
-      await kv.hmset(`chat:${id}`, payload)
-      await kv.zadd(`user:chat:${userId}`, {
-        score: createdAt,
-        member: `chat:${id}`
-      })
-    }
-  })
+  // const stream = OpenAIStream(res, {
+  //   async onCompletion(completion) {
+  //     const title = json.messages[0].content.substring(0, 100)
+  //     const id = json.id ?? nanoid()
+  //     const createdAt = Date.now()
+  //     const path = `/chat/${id}`
+  //     const payload = {
+  //       id,
+  //       title,
+  //       userId,
+  //       createdAt,
+  //       path,
+  //       messages: [
+  //         ...messages,
+  //         {
+  //           content: completion,
+  //           role: 'assistant'
+  //         }
+  //       ]
+  //     }
+  //     await kv.hmset(`chat:${id}`, payload)
+  //     await kv.zadd(`user:chat:${userId}`, {
+  //       score: createdAt,
+  //       member: `chat:${id}`
+  //     })
+  //   }
+  // })
 
-  return new StreamingTextResponse(stream)
+  // return new StreamingTextResponse(stream)
 }
